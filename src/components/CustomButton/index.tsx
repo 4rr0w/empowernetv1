7a6fb30f86796aa65106fc33af6561c994bb8e18
4rr0w <4rr0w.v@gmail.com>
2022-02-05 19:19:18 +0530

    Add custom button component

---
+++ src/components/CustomButton/index.tsx
@@ -0,0 +1,64 @@
+import { Button, ButtonProps as AntButtonProps } from 'antd';
+import { SizeType } from 'antd/lib/config-provider/SizeContext';
+import classnames from 'classnames';
+import React from 'react';
+import styles from './style.module.css';
+
+export interface ButtonProps extends AntButtonProps {
+  text?: string | React.ReactNode;
+  isSecondary?: boolean;
+  prefixIcon?: any;
+  gradient?: [string, string];
+  block?: boolean;
+  size?: SizeType;
+  round?: boolean;
+  style?: object;
+  ghost?: boolean;
+  iconRight?: boolean;
+  className?: string;
+  type?: 'text' | 'link' | 'ghost' | 'default' | 'primary' | 'dashed';
+}
+
+export const CustomButton: React.FC<ButtonProps> = ({
+  text = ' ',
+  isSecondary = false,
+  prefixIcon,
+  block = false,
+  gradient = ['#8F74FF', '#7B5CFE'],
+  round = false,
+  size = 'middle',
+  type = 'primary',
+  iconRight = false,
+  className = '',
+  style = {},
+  ...props
+}) => {
+  const iconRightStyle: object = {
+    display: iconRight ? 'inline-flex' : undefined,
+    alignItems: iconRight ? 'center' : undefined,
+    justifyContent: iconRight ? 'center' : undefined,
+    flexDirection: iconRight ? 'row-reverse' : undefined,
+  };
+  return (
+    <Button
+      className={classnames(styles.button, className)}
+      block={block}
+      type={isSecondary ? 'default' : type}
+      icon={prefixIcon}
+      style={{
+        backgroundImage: isSecondary
+          ? 'linear-gradient(180deg, #323740, #13161A)'
+          : `linear-gradient(180deg, ${gradient[0]},${gradient[1]})`,
+        height: size === 'middle' ? '36px' : '',
+        borderRadius: round ? '8px' : '',
+        ...style,
+        ...iconRightStyle,
+      }}
+      size={size}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    >
+      {text}
+    </Button>
+  );
+};
---
+++ src/stories/CustomButton.stories.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { MdSearch } from 'react-icons/md';
+import '../App.less';
+
+import { CustomButton, ButtonProps } from '../components/CustomButton';
+
+export default {
+  title: 'components/CustomButton',
+  component: CustomButton,
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <CustomButton {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Book a Demo',
+  size: 'middle',
+  isSecondary: false,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  isSecondary: true,
+  text: 'Sign Up',
+  size: 'large',
+};
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  prefixIcon: <MdSearch fontSize={27} style={{ marginLeft: '5px' }} />,
+  text: 'Find Mentor',
+  size: 'middle',
+  iconRight: true,
+  style: {
+    padding: '13px 16px',
+    height: '56px',
+    fontSize: '24px',
+  },
+};
+
+export const RoundButton = Template.bind({});
+RoundButton.args = {
+  prefixIcon: <MdSearch fontSize={24} />,
+  size: 'large',
+  round: true,
+  text: ' ',
+  style: {
+    height: '60px',
+    width: '80px',
+  },
+};
