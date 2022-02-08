d92636b314381aff9784458836b8c5f822444e1b
4rr0w <4rr0w.v@gmail.com>
2022-02-08 21:05:28 +0530

    add heading and search option in section 1

--- src/components/landingPage/Section1/index.tsx
+++ src/components/landingPage/Section1/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 import styles from './style.module.css';
 import Group51957 from '../../../assets/landingPage/Section1/Group 51957.png';
 import Group51855 from '../../../assets/landingPage/Section1/Group 51855.png';
 import Group51941 from '../../../assets/landingPage/Section1/Group 51941.png';
+import { CustomButton } from '../../CustomButton';
 
 export interface Section1Props {}
 
@@ -12,7 +14,36 @@ export const Section1: React.FC<Section1Props> = () => {
       <div className={styles.topContainer}>
         <img className={styles.graphics} alt="" src={Group51957} />
         <img className={styles.girl} alt="" src={Group51855} />
-        <img className={styles.hiddenTop} alt="" src={Group51855} />
+        <p className={styles.empowernet}>EMPOWERNET</p>
+        <p className={styles.subheading}>
+          Mentorship
+          <br />
+          without
+          <br />
+          borders
+        </p>
+        <div className={styles.inputContainer}>
+          <input
+            className={styles.searchInput}
+            placeholder="Try “Mathematics“ or “Computer Science”"
+          />
+          <CustomButton
+            className={styles.findButton}
+            iconRight
+            onClick={() => {}}
+            prefixIcon={
+              <MdSearch
+                fontSize={27}
+                style={{ marginLeft: '5px', fontSize: 'min(3vw, 34px)' }}
+              />
+            }
+            size="middle"
+            style={{
+              padding: '13px 16px',
+            }}
+            text="Find Mentor"
+          />
+        </div>
       </div>
       <div className={styles.bottomContainer}>
         <img className={styles.rocket} alt="" src={Group51941} />
