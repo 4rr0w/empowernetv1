16dc3a9bb4aed6c3779777f9e9221f04fea85264
4rr0w <4rr0w.v@gmail.com>
2022-02-09 20:23:01 +0530

    add text divs in section 3

--- src/components/landingPage/Section3/index.tsx
+++ src/components/landingPage/Section3/index.tsx
@@ -18,27 +18,68 @@ export const Section3: React.FC<Section3Props> = () => {
   return (
     <div className={styles.mainSec3}>
       <div className={styles.skillsContainer}>
+        <p className={styles.heading}>WHY EMPOWERNET?</p>
         <img className={styles.cshape3} alt="" src={cshape3} />
         <img className={styles.skills} alt="" src={Group51958} />
         <img className={styles.girl} alt="" src={Group51906} />
-        {/* <img className={styles.cshape3Hidden} alt="" src={cshape3} /> */}
+        <div className={styles.skillsText}>
+          <span className={styles.subheading}>Skills Of Interest</span>
+          <p className={styles.para}>
+            Capture quantitative skills of interest, or those complimentary to
+            academic or career subjects.
+          </p>
+        </div>
       </div>
       <div className={styles.mentorsContainer}>
         <img className={styles.group} alt="" src={Group51931} />
-        {/* <img className={styles.groupHidden} alt="" src={Group51931} /> */}
+        <div className={styles.mentorsText}>
+          <span className={styles.subheading}>Female Mentors</span>
+          <p className={styles.para}>
+            Address crucial quantitative skill gaps with personalized mentorship
+            from female mentors, both domestically and worldwide.
+          </p>
+        </div>
       </div>
       <div className={styles.directSupportContainer}>
         <img className={styles.cshape} alt="" src={cshape} />
         <img className={styles.window} alt="" src={Group51960} />
-        {/* <img className={styles.cshapeHidden} alt="" src={cshape} /> */}
+        <div className={styles.directSupportText}>
+          <span className={styles.subheading}>Direct Skill Support</span>
+          <p className={styles.para}>
+            Access detailed resource documents in distinct subjects that provide
+            more direct skill support.
+            <br /> <br />
+            Drop in any question in the Q+A chat box and receive high-quality,
+            reliable information from experts across the Empowernet community,
+            and follow topics that interest you.
+          </p>
+        </div>
       </div>
       <div className={styles.languageContainer}>
         <img className={styles.cshape1} alt="" src={cshape1} />
         <img className={styles.ftfwindow} alt="" src={Group51958} />
+        <div className={styles.languageText}>
+          <span className={styles.subheading}>
+            Talk In Your Language Face-To-Faces
+          </span>
+          <p className={styles.para}>
+            Choose the main language of the app and mentors in the language you
+            feel most comfortable with and talk to them face-to-face for the
+            time they are available.
+          </p>
+        </div>
       </div>
       <div className={styles.reliableContainer}>
         <img className={styles.cshape2} alt="" src={cshape2} />
         <img className={styles.infowindow} alt="" src={Group51962} />
+        <div className={styles.reliableText}>
+          <span className={styles.subheading}>Reliable Information</span>
+          <p className={styles.para}>
+            Drop in any question in the Q+A chat box and receive high-quality,
+            reliable information from experts across the Empowernet community,
+            and follow topics that interest you.
+          </p>
+        </div>
       </div>
     </div>
   );
