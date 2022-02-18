7ade9b19eac51a0e5ba1377c496350520aac256f
4rr0w <4rr0w.v@gmail.com>
2022-02-18 20:20:48 +0530

    add signup flow

--- src/screens/SignUp/AboutMePage/index.tsx
+++ src/screens/SignUp/AboutMePage/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import {
-  MdAdd,
-  MdLanguage,
-  MdLocationCity,
-  MdMap,
-  MdPhone,
-  MdPlace,
-  MdSchool,
-} from 'react-icons/md';
+import { MdAdd, MdLanguage, MdPlace, MdSchool, MdWork } from 'react-icons/md';
+import { CustomButton } from '../../../components/CustomButton';
 import { InputWithIcon } from '../../../components/InputWithIcon';
 import styles from './style.module.css';
 
-export interface AboutMePageProps {}
+export interface AboutMePageProps {
+  getRef?: Function;
+  mentor?: boolean;
+  onNextClick?: Function;
+}
 
-export const AboutMePage: React.FC<AboutMePageProps> = () => {
+export const AboutMePage: React.FC<AboutMePageProps> = ({
+  getRef = () => null,
+  mentor = false,
+  onNextClick = () => null,
+}) => {
+  const ref = React.useRef(null);
+  React.useEffect(() => {
+    getRef(ref);
+  }, [ref]);
   return (
     <div className={styles.container}>
       <span className={styles.heading}>About me</span>
@@ -34,10 +39,9 @@ export const AboutMePage: React.FC<AboutMePageProps> = () => {
             onFocusColor="rgba(255, 215, 20, 1)"
             placeholder="Last Name"
           />
-          <div className={styles.box} />
         </div>
 
-        <div className={styles.row}>
+        {/* <div className={styles.row}>
           <InputWithIcon
             className={styles.input}
             icon={<MdLocationCity style={{ fontSize: 'min(25px, 4vw)' }} />}
@@ -51,31 +55,50 @@ export const AboutMePage: React.FC<AboutMePageProps> = () => {
             placeholder="State"
           />
           <div className={styles.box} />
-        </div>
+        </div> */}
         <InputWithIcon
           className={styles.input}
           icon={<MdPlace style={{ fontSize: 'min(25px, 4vw)' }} />}
           onFocusColor="rgba(255, 215, 20, 1)"
           placeholder="Country"
         />
-        <InputWithIcon
+        {/* <InputWithIcon
           className={styles.input}
           icon={<MdPhone style={{ fontSize: 'min(25px, 4vw)' }} />}
           onFocusColor="rgba(255, 215, 20, 1)"
           placeholder="Phone"
-        />
+        /> */}
         <InputWithIcon
           className={styles.input}
           icon={<MdSchool style={{ fontSize: 'min(25px, 4vw)' }} />}
           onFocusColor="rgba(255, 215, 20, 1)"
-          placeholder="Education"
+          placeholder="Highest Education"
         />
+        {mentor && (
+          <InputWithIcon
+            className={styles.input}
+            icon={<MdWork style={{ fontSize: 'min(25px, 4vw)' }} />}
+            onFocusColor="rgba(255, 215, 20, 1)"
+            placeholder="Profession"
+          />
+        )}
 
         <InputWithIcon
           className={styles.input}
           icon={<MdLanguage style={{ fontSize: 'min(25px, 4vw)' }} />}
           onFocusColor="rgba(255, 215, 20, 1)"
           placeholder="Languages"
+        />
+        <CustomButton
+          block
+          onClick={() => onNextClick()}
+          size="large"
+          text="Next"
+          style={{
+            marginTop: '2vw',
+
+            fontSize: 'min(16px,2vw)',
+          }}
         />
       </div>
     </div>
