6e76b2814750f580eee17d41376c23b2629f4dd4
4rr0w <4rr0w.v@gmail.com>
2022-02-21 11:55:59 +0530

    add responsiveness to mentor card

--- src/components/MentorCard/index.tsx
+++ src/components/MentorCard/index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import {
   StarFilled,
-  DeleteFilled,
   HeartFilled,
   WechatFilled,
   PhoneFilled,
@@ -13,91 +12,96 @@ import {
 } from '@ant-design/icons';
 import styles from './style.module.css';
 import Maskgrp2 from '../../assets/landingPage/Section6/MaskGroup2.png';
+import { CustomButton } from '../CustomButton';
 
 export interface MentorCardProps {
-  a?: { name: string; info: string; img_link: string };
+  mentor?: {
+    name: string;
+    education: string;
+    profession: string;
+    bio: string;
+    img_link: string;
+    skills: string[];
+    stars: number;
+  };
 }
 
 export const MentorCard: React.FC<MentorCardProps> = ({
-  a = {
+  mentor = {
     name: 'Fredia Gibbs',
-    info: 'With a decade of product management experience in companies across Europe and the US, I am hoping to share my knowledge and support to other product people by creating a tailored coaching program according to needs and goals.',
+    education: 'Btech',
+    profession: 'Product Manager',
+    skills: ['Python', 'Cpp'],
+    bio: 'With a decade of product management experience in companies across Europe and the US, I am hoping to share my knowledge and support to other product people by creating a tailored coaching program according to needs and goals.',
     img_link: '../../assets/landingPage/Section6/MaskGroup2.png',
+    stars: 4,
   },
 }) => {
   return (
     <div className={styles.mainSec}>
       <div className={styles.container}>
-        <div className={styles.profile}>
-          <div className={styles.bio}>
-            <div>
-              <img className={styles.img} src={Maskgrp2} />
-            </div>
-            <div className={styles.container2}>
-              <div className={styles.desc}>
-                <div>
-                  <p className={styles.name}>{a.name} &nbsp;</p>
-                </div>
-                <div>
-                  <p className={styles.textshort}>
-                    &nbsp; &ensp; &nbsp; &ensp;English
-                  </p>
-                </div>
-                <div>
-                  <DeleteFilled />
-                </div>
-                <div>
-                  <StarFilled />
-                </div>
-              </div>
-
-              <p className={styles.text2}>
-                <b>Head of product</b>- Search & Find-ability at <b>ABD</b>
-              </p>
+        <div className={styles.header}>
+          <img className={styles.img} src={Maskgrp2} />
+          <div className={styles.desc}>
+            <p className={styles.name}>
+              {mentor.name}
               <div className={styles.stars}>
-                <div>
-                  <StarFilled />
-                </div>
-                <div>
-                  <StarFilled />
-                </div>
-                <div>
-                  <StarFilled />
-                </div>
-                <div>
-                  <StarFilled />
-                </div>
-                <div>
-                  <StarFilled />
-                </div>
+                <StarFilled />
+                <StarFilled />
+                <StarFilled />
+                <StarFilled />
               </div>
-            </div>
-          </div>
-          <div>buttons</div>
-          <div>
-            <p className={styles.info}>
-              {a.info}
-              <br />
-              <br />
-              {a.info}
+            </p>
+            <p className={styles.text2}>
+              <b>Head of product</b>- Search & Find-ability at <b>ABD</b>
             </p>
           </div>
         </div>
-        <div className={styles.contact}>
-          <HeartFilled className={styles.sideicon1} />
+        <div className={styles.profile}>
+          <div className={styles.tags}>
+            {mentor.skills.map((tag) => {
+              return (
+                <CustomButton
+                  className={styles.tag}
+                  onClick={() => {}}
+                  size="middle"
+                  text={tag}
+                />
+              );
+            })}
+          </div>
+          <p className={styles.bio}>{mentor.bio}</p>
+        </div>
+      </div>
+      <div className={styles.contact}>
+        <div>
+          <HeartFilled className={styles.sideicon} />
           <p className={styles.texticon}> like</p>
+        </div>
+        <div>
           <WechatFilled className={styles.sideicon} />
           <p className={styles.texticon}> Chat</p>
+        </div>
+        <div>
           <PhoneFilled className={styles.sideicon} />
           <p className={styles.texticon}>Call</p>
+        </div>
+        <div>
           <VideoCameraFilled className={styles.sideicon} />
           <p className={styles.texticon}>Video Call</p>
+        </div>
+        <div>
           <CheckSquareOutlined className={styles.sideicon} />
           <p className={styles.texticon}>Tasks</p>
+        </div>
+        <div>
           <ShoppingFilled className={styles.sideicon} />
           <p className={styles.texticon}>Hands on</p>
+        </div>
+        <div>
+          {' '}
           <FileAddFilled className={styles.sideicon} />
-          <p className={styles.texticon}>Resources</p>
+          <p className={styles.texticon}>Resources</p>{' '}
         </div>
       </div>
     </div>
